0877d789f4a3669b51339ed64fbb5215df8fc017
jiyuu-jin <zach@valist.io>
2023-11-05 21:39:03 -0500

    Add ring handlers

---
+++ src/handlers.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+use axum::extract::State;
+use crate::utils::RingRestClient;
+use crate::types::LocationsRes;
+
+pub async fn ring_handler(State(ring_rest_client): State<Arc<RingRestClient>>) -> String {
+   let locations_res = ring_rest_client.get_locations().await;
+   let locations = serde_json::from_str::<LocationsRes>(&locations_res).expect(&format!("locations_res: {locations_res}"));
+   for location in locations.user_locations {
+      println!("{}", location.location_id)
+   }
+
+   let devices_res = ring_rest_client.get_devices().await;
+   let socket_token = ring_rest_client.get_socket_ticket().await;
+
+   return socket_token;
+}
+
+pub async fn ring_auth_handler(State(ring_rest_client): State<Arc<RingRestClient>>) -> String {
+   ring_rest_client.request_auth_token().await
+}
