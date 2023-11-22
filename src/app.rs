a0ff1a8a51efd26027d3917389e44b120d26e2f5
Chris Smith <chris@smith.xyz>
2023-11-22 18:31:40 -0500

    fix: random text on page

--- src/app.rs
+++ src/app.rs
@@ -11,8 +11,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        /// injects a stylesheet into the document <head>
-        /// id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/{{project-name}}.css"/>
 
         // sets the document title
