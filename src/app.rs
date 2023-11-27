f8b850f78ec41563e85e6a319f9364bd4e388fd5
Chris Smith <chris@smith.xyz>
2023-11-27 00:28:25 -0500

    Prototype RTC

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,16 @@
 use {
     crate::error_template::{AppError, ErrorTemplate},
     base64::{engine::general_purpose::STANDARD as base64, Engine},
+    js_sys::Reflect,
     leptos::*,
     leptos_meta::*,
+    leptos_reactive::create_effect,
     leptos_router::*,
+    leptos_use::{core::ConnectionReadyState, use_websocket, UseWebsocketReturn},
     serde::{Deserialize, Serialize},
+    serde_json::json,
+    wasm_bindgen::{closure::Closure, JsValue},
+    web_sys::RtcPeerConnection,
 };
 
 #[component]
@@ -31,6 +37,7 @@ pub fn App() -> impl IntoView {
                     <Route path="" view=HomePage/>
                     <Route path="/login" view=LoginPage/>
                     <Route path="/ring" view=RingPage/>
+                    <Route path="/websocket" view=WebSocketPage/>
                 </Routes>
             </main>
         </Router>
@@ -45,14 +52,15 @@ fn HomePage() -> impl IntoView {
             <a href="/login">Login</a>
         </p>
         <p>
-            <a href="/ring" rel="external">
-                "Ring"
-            </a>
+            <A href="/ring">"Ring"</A>
         </p>
         <p>
             <a href="/api/roku" rel="external">
                 "Roku"
             </a>
+        </p>
+        <p>
+            <A href="/websocket">"WebSocket"</A>
         </p>
     }
 }
@@ -88,7 +96,7 @@ fn LoginPage() -> impl IntoView {
         </ActionForm>
         <p>{value}</p>
         <p>
-            <A href="/ring">"Ring"</A>
+            <A href="/">"Home"</A>
         </p>
     }
 }
@@ -109,6 +117,7 @@ pub struct RingCameraSnapshot {
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct RingCamera {
+    pub id: u64,
     pub description: String,
     pub snapshot: RingCameraSnapshot,
     pub health: u64,
@@ -153,6 +162,7 @@ pub async fn get_ring_values() -> Result<RingValues, ServerFnError> {
     Ok(RingValues {
         location_name: location.name,
         front_camera: RingCamera {
+            id: front_camera.id,
             description: front_camera.description,
             snapshot: RingCameraSnapshot {
                 image: front_image_base64,
@@ -161,6 +171,7 @@ pub async fn get_ring_values() -> Result<RingValues, ServerFnError> {
             health: front_camera.health.battery_percentage,
         },
         back_camera: RingCamera {
+            id: back_camera.id,
             description: back_camera.description,
             snapshot: RingCameraSnapshot {
                 image: back_image_base64,
@@ -191,7 +202,10 @@ fn RingPage() -> impl IntoView {
                                 <h1>{data.location_name}</h1>
                                 <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(300px, max-content)); grid-gap: 8px">
                                     <div>
-                                        <h2>{data.front_camera.description} - Battery: {data.front_camera.health}</h2>
+                                        <h2>
+                                            {data.front_camera.description} - Battery:
+                                            {data.front_camera.health}
+                                        </h2>
                                         <img
                                             style="width: 100%"
                                             src=format!(
@@ -210,7 +224,10 @@ fn RingPage() -> impl IntoView {
                                         {}
                                     </div>
                                     <div>
-                                        <h2>{data.back_camera.description} - Battery: {data.back_camera.health}</h2>
+                                        <h2>
+                                            {data.back_camera.description} - Battery:
+                                            {data.back_camera.health}
+                                        </h2>
                                         <img
                                             style="width: 100%"
                                             src=format!(
@@ -225,6 +242,7 @@ fn RingPage() -> impl IntoView {
                                             <li>{} - {}</li>
                                         </ul>
                                         <h2>Recordings</h2>
+
                                         {}
                                     </div>
                                 </div>
@@ -235,6 +253,7 @@ fn RingPage() -> impl IntoView {
                         })
                     })
             }}
+
         </Suspense>
     }
 }
@@ -285,6 +304,115 @@ fn RokuPage() -> impl IntoView {
                     <option value="2">Device 2</option>
                 </select>
             </div>
+        </div>
+    }
+}
+
+#[component]
+fn WebSocketPage() -> impl IntoView {
+    let ring_values = create_resource(|| (), |_| get_ring_values());
+
+    view! {
+        <h1>"WebSocket"</h1>
+        <Suspense fallback=move || {
+            view! { <p>"Loading..."</p> }
+        }>
+            {move || {
+                ring_values
+                    .get()
+                    .map(|ring_values| {
+                        ring_values
+                            .map(|ring_values| {
+                                view! { <WebSocketComponent ring_values=ring_values/> }
+                            })
+                    })
+            }}
+
+        </Suspense>
+    }
+}
+
+#[component]
+fn WebSocketComponent(ring_values: RingValues) -> impl IntoView {
+    let UseWebsocketReturn {
+        ready_state,
+        message,
+        message_bytes,
+        send,
+        send_bytes,
+        open,
+        close,
+        ..
+    } = use_websocket(&ring_values.ws_url);
+
+    let send_message = move |_| {
+        send("Hello, world!");
+    };
+
+    // let send_byte_message = move |_| {
+    //     send_bytes(b"Hello, world!\r\n".to_vec());
+    // };
+
+    create_effect(move |_| {
+        if ready_state.get() == ConnectionReadyState::Open {
+            let send_bytes = send_bytes.clone();
+            let pc = RtcPeerConnection::new().unwrap();
+            let create_offer_callback = Closure::wrap(Box::new(move |offer: JsValue| {
+                let sdp = Reflect::get(&offer, &JsValue::from_str("sdp"))
+                    .unwrap()
+                    .as_string()
+                    .unwrap();
+                send_bytes(
+                    serde_json::to_vec(&json!({
+                        "method": "live_view",
+                        "dialog_id": "333333",
+                        "body": {
+                            "doorbot_id": ring_values.front_camera.id,
+                            "stream_options": { "audio_enabled": true, "video_enabled": true },
+                            "sdp": sdp,
+                        }
+                    }))
+                    .unwrap(),
+                );
+            }) as Box<dyn FnMut(JsValue)>);
+            let _ = pc.create_offer().then(&create_offer_callback);
+            create_offer_callback.forget();
+        }
+    });
+    let status = move || ready_state.get().to_string();
+
+    let connected = move || ready_state.get() == ConnectionReadyState::Open;
+
+    let open_connection = move |_| {
+        open();
+    };
+
+    let close_connection = move |_| {
+        close();
+    };
+
+    view! {
+        <div>
+            <p>"status: " {status}</p>
+
+            <button on:click=send_message disabled=move || !connected()>
+                "Send"
+            </button>
+            // <button on:click=send_byte_message disabled=move || !connected()>
+            // "Send bytes"
+            // </button>
+            <button on:click=open_connection disabled=connected>
+                "Open"
+            </button>
+            <button on:click=close_connection disabled=move || !connected()>
+                "Close"
+            </button>
+
+            <h2>"Receive message: "</h2>
+            <pre style="text-wrap: wrap; word-break: break-all;">
+                {move || format!("{:?}", message.get())}
+            </pre>
+            <p>"Receive byte message: " {move || format!("{:?}", message_bytes.get())}</p>
         </div>
     }
 }
