148179a4d361c9bf5648cd67df7f8edf24f54433
jiyuu-jin <zach@valist.io>
2023-12-02 17:41:32 -0800

    Refactor discover_roku to return devices

--- src/integrations/roku/client/mod.rs
+++ src/integrations/roku/client/mod.rs
@@ -1,24 +1,27 @@
+use super::types::RokuDiscoverRes;
+
 use {futures::prelude::*, ssdp_client::SearchTarget, std::time::Duration};
 
-pub async fn discover_roku() {
+pub async fn discover_roku() -> Vec<RokuDiscoverRes> {
     let search_target = SearchTarget::RootDevice;
     let mut responses = ssdp_client::search(&search_target, Duration::from_secs(3), 2, None)
         .await
         .unwrap();
+    let mut devices = Vec::with_capacity(20);
 
     while let Some(response) = responses.next().await {
         match response {
-            Ok(resp) => {
-                println!("Location: {}", resp.location());
-                println!("USN: {}", resp.usn());
-                println!("Server: {}", resp.server());
-                println!("------------------------");
-            }
+            Ok(resp) => devices.push(RokuDiscoverRes {
+                location: resp.location().to_string(),
+                usn: resp.usn().to_string(),
+                server: resp.server().to_string(),
+            }),
             Err(e) => {
                 println!("Error: {:?}", e);
             }
         }
     }
+    devices
 }
 
 pub async fn get_roku_apps() -> String {
