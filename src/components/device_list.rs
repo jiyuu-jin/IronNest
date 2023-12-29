fc36f5347395a5ec44ae0a2b339e2621189a8161
jiyuu-jin <zach@valist.io>
2023-12-28 22:22:27 -0500

    Update device_list styles

--- src/components/device_list.rs
+++ src/components/device_list.rs
@@ -3,41 +3,42 @@ use {crate::integrations::ring::types::RingValues, leptos::*};
 #[component]
 pub fn DeviceList(ring_values: Resource<(), Result<RingValues, ServerFnError>>) -> impl IntoView {
     view! {
-        <h2 class="text-lg">"Devices"</h2>
-        <hr/>
-        <Suspense fallback=|| {
-            view! { <p>"Loading devices..."</p> }
-        }>
-            {move || {
-                ring_values
-                    .get()
-                    .map(|data| {
-                        data.map(|data| {
-                            view! {
-                                <ul class="tplink-device-list space-y-2">
-                                    {data
-                                        .devices
-                                        .iter()
-                                        .map(|device| {
-                                            view! {
-                                                <li class="tplink-device">
-                                                    <div class="device-alias">
-                                                        {&device.name} {format!("({})", &device.device_type)}
-                                                    </div>
-                                                    <div class="device-name">{&device.ip}</div>
-                                                    <div class="device-state">
-                                                        {format!("State: {}", &device.state)}
-                                                    </div>
-                                                </li>
-                                            }
-                                        })
-                                        .collect::<Vec<_>>()}
-                                </ul>
-                            }
+        <div>
+            <h2 class="text-lg">"Devices"</h2>
+            <hr class="mb-2" />
+            <Suspense fallback=|| {
+                view! { <p>"Loading devices..."</p> }
+            }>
+                {move || {
+                    ring_values
+                        .get()
+                        .map(|data| {
+                            data.map(|data| {
+                                view! {
+                                    <ul class="device-list space-y-2">
+                                        {data
+                                            .devices
+                                            .iter()
+                                            .map(|device| {
+                                                view! {
+                                                    <li class="rounded-lg bg-white shadow p-2">
+                                                        <div class="device-alias">
+                                                            {&device.name} {format!(" ({})", &device.device_type)}
+                                                        </div>
+                                                        <div class="device-name">{&device.ip}</div>
+                                                        <div class="device-state">
+                                                            {format!("State: {}", &device.state)}
+                                                        </div>
+                                                    </li>
+                                                }
+                                            })
+                                            .collect::<Vec<_>>()}
+                                    </ul>
+                                }
+                            })
                         })
-                    })
-            }}
-
-        </Suspense>
+                }}
+            </Suspense>
+        </div>
     }
 }
