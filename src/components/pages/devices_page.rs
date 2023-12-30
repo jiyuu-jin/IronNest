b2b27e593989cd72f398f2242feee2297ffe3c80
jiyuu-jin <zach@valist.io>
2023-12-29 23:25:55 -0500

    Add devices table

--- src/components/pages/devices_page.rs
+++ src/components/pages/devices_page.rs
@@ -18,6 +18,10 @@ pub async fn get_devices() -> Result<Vec<Device>, ServerFnError> {
     for row in rows {
         let state_value: i64 = row.get("power_state");
         let state: u8 = state_value.try_into().expect("Value out of range for u8");
+        // let battery_percentage_value: i64 = row.get("battery_percentage");
+        // let battery_percentage: u64 = battery_percentage_value
+        //     .try_into()
+        //     .expect("Value out of range for u64");
 
         devices.push(Device {
             id: row.get("id"),
@@ -25,6 +29,7 @@ pub async fn get_devices() -> Result<Vec<Device>, ServerFnError> {
             device_type: row.get("device_type"),
             ip: row.get("ip"),
             state,
+            battery_percentage: 0,
         });
     }
 
@@ -35,55 +40,93 @@ pub async fn get_devices() -> Result<Vec<Device>, ServerFnError> {
 pub fn DevicesPage() -> impl IntoView {
     let devices = create_resource(|| (), |_| get_devices());
     view! {
-      <main class="lg:pl-20">
-        <div class="lg:pl-4 -mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table class="w-full divide-y divide-gray-300">
-              <thead>
-                <tr>
-                  <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-0">Name</th>
-                  <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">"Device Type"</th>
-                  <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">"IP"</th>
-                  <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">"Power State"</th>
-                  <th scope="col" class="relative py-3 pl-3 pr-4 sm:pr-0">
-                    <span class="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-gray-200 bg-white">
-                    <Suspense fallback=|| {
-                      view! { <p>"Loading devices..."</p> }
-                    }>
-                      {move || {
-                          devices
-                              .get()
-                              .map(|data| {
-                                  data.map(|data| {
-                                      view! {
-                                        <>
-                                              {data
-                                                  .iter()
-                                                  .map(|device| {
-                                                      view! {
-                                                          <tr>
-                                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{&device.name}</td>
-                                                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">{&device.device_type}</th>
-                                                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">{&device.ip}</th>
-                                                            <th scope="col" class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500">{device.state}</th>
-                                                          </tr>
-                                                      }
-                                                  })
-                                                  .collect::<Vec<_>>()}
-                                          </>
-                                      }
-                                  })
-                              })
-                      }}
-                  </Suspense>
-                </tbody>
-              </table>
+        <main class="lg:pl-20">
+            <div class="lg:pl-4 -mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table class="w-full divide-y divide-gray-300">
+                        <thead>
+                            <tr>
+                                <th
+                                    scope="col"
+                                    class="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-0"
+                                >
+                                    Name
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                                >
+                                    "Device Type"
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                                >
+                                    "IP"
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                                >
+                                    "Power State"
+                                </th>
+                                <th scope="col" class="relative py-3 pl-3 pr-4 sm:pr-0">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 bg-white">
+                            <Suspense fallback=|| {
+                                view! { <p>"Loading devices..."</p> }
+                            }>
+                                {move || {
+                                    devices
+                                        .get()
+                                        .map(|data| {
+                                            data.map(|data| {
+                                                view! {
+                                                    <>
+                                                        {data
+                                                            .iter()
+                                                            .map(|device| {
+                                                                view! {
+                                                                    <tr>
+                                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                                                            {&device.name}
+                                                                        </td>
+                                                                        <th
+                                                                            scope="col"
+                                                                            class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                                                                        >
+                                                                            {&device.device_type}
+                                                                        </th>
+                                                                        <th
+                                                                            scope="col"
+                                                                            class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                                                                        >
+                                                                            {&device.ip}
+                                                                        </th>
+                                                                        <th
+                                                                            scope="col"
+                                                                            class="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                                                                        >
+                                                                            {device.state}
+                                                                        </th>
+                                                                    </tr>
+                                                                }
+                                                            })
+                                                            .collect::<Vec<_>>()}
+                                                    </>
+                                                }
+                                            })
+                                        })
+                                }}
+
+                            </Suspense>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
         </main>
     }
 }
