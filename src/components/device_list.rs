6a3246f969f2171d70dccd6eb4257d3858725910
jiyuu-jin <zach@valist.io>
2024-01-06 19:01:46 -0500

    Add DeviceItems for DeviceList device types

--- src/components/device_list.rs
+++ src/components/device_list.rs
@@ -1,3 +1,5 @@
+use crate::integrations::iron_nest::types::DeviceType;
+
 use {
     super::pages::dashboard_page::DashboardValues, crate::integrations::iron_nest::types::Device,
     leptos::*,
@@ -39,43 +41,22 @@ pub fn DeviceList(
     }
 }
 
-pub enum DeviceType {
-    SmartPlug,
-    SmartLight,
-    RingDoorbell,
-}
-
-impl DeviceType {
-    fn from_str(device_type: &str) -> Option<DeviceType> {
-        match device_type {
-            "smart-plug" => Some(DeviceType::SmartPlug),
-            "smart-light" => Some(DeviceType::SmartLight),
-            "ring-doorbell" => Some(DeviceType::RingDoorbell),
-            _ => None,
-        }
-    }
-}
-
 #[component]
 pub fn DeviceListItem(device: Device) -> impl IntoView {
-    if let Some(device_type) = DeviceType::from_str(&device.device_type) {
-        match device_type {
-            DeviceType::SmartPlug => view! { <SmartPlugItem device=device/> },
-            DeviceType::SmartLight => view! { <SmartLightItem device=device/> },
-            DeviceType::RingDoorbell => view! { <SmartPlugItem device=device/> },
-        }
-    } else {
-        view! { <SmartPlugItem device=device/> }
+    match device.device_type {
+        DeviceType::SmartPlug => view! { <SmartPlugItem device=device/> },
+        DeviceType::SmartLight => view! { <SmartLightItem device=device/> },
+        DeviceType::RingDoorbell => view! { <RingDoorbellItem device=device/> },
     }
 }
 
 #[server(HandleSmartPlugToggle)]
-pub async fn handle_smart_plug_toggle(state: u8, ip: String) -> Result<(), ServerFnError> {
+pub async fn handle_smart_plug_toggle(state: bool, ip: String) -> Result<(), ServerFnError> {
     use crate::integrations::tplink::{tplink_turn_plug_off, tplink_turn_plug_on};
-    if state == 0 {
-        tplink_turn_plug_on(&ip).await;
-    } else {
+    if state {
         tplink_turn_plug_off(&ip).await;
+    } else {
+        tplink_turn_plug_on(&ip).await;
     }
     Ok(())
 }
@@ -83,30 +64,33 @@ pub async fn handle_smart_plug_toggle(state: u8, ip: String) -> Result<(), Serve
 #[component]
 pub fn SmartPlugItem(device: Device) -> impl IntoView {
     let ip = device.ip.to_string();
-    let state = device.state;
+    let (signal, set_signal) = create_signal(device.power_state == 1);
 
     view! {
-        <li class="rounded-lg bg-white shadow p-2">
-            <div class="device-alias">{&device.name} {format!("({})", &device.device_type)}</div>
-            <div class="device-name">{&device.ip}</div>
-            <div class="device-state">
-                {format!("State: {}", &device.state)}
-                <label class="relative inline-flex items-center cursor-pointer ml-2 mt-2">
+        <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+            <div class="flex w-full items-center justify-between space-x-6 p-6">
+            <div class="flex-1 truncate">
+                <div class="flex items-center space-x-3">
+                <h3 class="truncate text-sm font-medium text-gray-900">{&device.name}</h3>
+                <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{format!("{}", &device.device_type)}</span>
+                </div>
+                <p class="mt-1 truncate text-sm text-gray-500">{&device.ip}</p>
+            </div>
+            <label class="relative inline-flex items-center cursor-pointer ml-2 mt-2">
                     <input
                         type="checkbox"
                         value=""
                         on:click=move |_| {
                             let ip_clone = ip.clone();
-                            let state_clone = state.clone();
                             println!("clicked!");
                             spawn_local(async move {
-                                handle_smart_plug_toggle(state_clone.clone(), ip_clone)
+                                handle_smart_plug_toggle(signal.get(), ip_clone)
                                     .await
                                     .unwrap();
+                                set_signal.set(!signal.get());
                             });
                         }
-
-                        checked=device.state == 1
+                        checked=signal
                         class="sr-only peer"
                     />
                     <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
@@ -116,40 +100,60 @@ pub fn SmartPlugItem(device: Device) -> impl IntoView {
     }
 }
 
+#[component]
+pub fn RingDoorbellItem(device: Device) -> impl IntoView {
+    view! {
+        <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+            <div class="flex w-full items-center justify-between space-x-6 p-6">
+                <div class="flex-1 truncate">
+                    <div class="flex items-center space-x-3">
+                        <h3 class="truncate text-sm font-medium text-gray-900">{&device.name}</h3>
+                        <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{format!("{}", &device.device_type)}</span>
+                    </div>
+                    <p class="mt-1 truncate text-sm text-gray-500">{format!("Battery: {}", &device.battery_percentage)}</p>
+                </div>
+            </div>
+        </li>
+    }
+}
+
 #[server(HandleSmartLightToggle)]
-pub async fn handle_smart_light_toggle(state: u8, ip: String) -> Result<(), ServerFnError> {
+pub async fn handle_smart_light_toggle(state: bool, ip: String) -> Result<(), ServerFnError> {
     use crate::integrations::tplink::tplink_turn_light_on_off;
-    tplink_turn_light_on_off(&ip, if state == 1 { 0 } else { 1 }).await;
+    tplink_turn_light_on_off(&ip, if state { 0 } else { 1 }).await;
     Ok(())
 }
 
 #[component]
 pub fn SmartLightItem(device: Device) -> impl IntoView {
     let ip = device.ip.to_string();
-    let state = device.state;
+    let (signal, set_signal) = create_signal(device.power_state == 1);
 
     view! {
-        <li class="rounded-lg bg-white shadow p-2">
-            <div class="device-alias">{&device.name} {format!("({})", &device.device_type)}</div>
-            <div class="device-name">{&device.ip}</div>
-            <div class="device-state">
-                {format!("State: {}", &device.state)}
-                <label class="relative inline-flex items-center cursor-pointer ml-2 mt-2">
+        <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+            <div class="flex w-full items-center justify-between space-x-6 p-6">
+            <div class="flex-1 truncate">
+                <div class="flex items-center space-x-3">
+                <h3 class="truncate text-sm font-medium text-gray-900">{&device.name}</h3>
+                <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{format!("{}", &device.device_type)}</span>
+                </div>
+                <p class="mt-1 truncate text-sm text-gray-500">{&device.ip}</p>
+            </div>
+            <label class="relative inline-flex items-center cursor-pointer ml-2 mt-2">
                     <input
                         type="checkbox"
                         value=""
                         on:click=move |_| {
                             let ip_clone = ip.clone();
-                            let state_clone = state.clone();
                             println!("clicked!");
                             spawn_local(async move {
-                                handle_smart_light_toggle(state_clone.clone(), ip_clone)
+                                handle_smart_plug_toggle(signal.get(), ip_clone)
                                     .await
                                     .unwrap();
+                                set_signal.set(!signal.get());
                             });
                         }
-
-                        checked=device.state == 1
+                        checked=signal
                         class="sr-only peer"
                     />
                     <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
