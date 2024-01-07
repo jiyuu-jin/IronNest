d331f2fd81c2cf62c69cef38097aa80b945bbe40
jiyuu-jin <zach@valist.io>
2024-01-06 19:02:06 -0500

    Add sorting and enum changes to dashboard

--- src/components/pages/dashboard_page.rs
+++ src/components/pages/dashboard_page.rs
@@ -1,7 +1,10 @@
 use {
     crate::{
         components::{command_box::CommandBox, device_list::DeviceList, ring_cameras::RingCameras},
-        integrations::{iron_nest::types::Device, ring::types::RingCamera},
+        integrations::{
+            iron_nest::types::Device,
+            ring::types::{RingCamera, RingCameraSnapshot, VideoSearchRes},
+        },
     },
     leptos::*,
     serde::{Deserialize, Serialize},
@@ -19,64 +22,47 @@ pub struct DashboardValues {
 #[server(GetDashboardValues)]
 pub async fn get_dashboard_values() -> Result<DashboardValues, ServerFnError> {
     use {
-        crate::integrations::{
-            iron_nest::types::Device,
-            ring::{client::RingRestClient, get_ring_camera},
-        },
+        crate::integrations::iron_nest::types::Device,
         sqlx::{Pool, Row, Sqlite},
     };
 
-    let ring_rest_client = use_context::<Arc<RingRestClient>>().unwrap();
     let pool = use_context::<Arc<Pool<Sqlite>>>().unwrap();
 
-    let rows = sqlx::query(
-        "SELECT id, name, device_type, ip, power_state, battery_percentage FROM devices",
+    let query = "
+        SELECT id, name, device_type, ip, power_state, battery_percentage
+        FROM devices
+        ORDER BY name
+    ";
+    let devices = sqlx::query_as::<Sqlite, Device>(query)
+        .fetch_all(&*pool)
+        .await?;
+
+    let ring_camera_rows = sqlx::query(
+        "SELECT id, description, snapshot_image, snapshot_timestamp, health FROM ring_cameras",
     )
     .fetch_all(&*pool)
     .await?;
 
-    let mut devices = Vec::new();
-    for row in rows {
-        let state_value: u8 = row.get("power_state");
-        let state: u8 = state_value.try_into().expect("Value out of range for u8");
-        let battery_percentage_value: i64 = row.get("battery_percentage");
-        let battery_percentage: u64 = battery_percentage_value
-            .try_into()
-            .expect("Value out of range for u64");
-
-        devices.push(Device {
-            id: row.get("id"),
-            name: row.get("name"),
-            device_type: row.get("device_type"),
-            ip: row.get("ip"),
-            state,
-            battery_percentage: battery_percentage,
+    let mut cameras = Vec::new();
+    for ring_camera_row in ring_camera_rows {
+        cameras.push(RingCamera {
+            id: ring_camera_row.get("id"),
+            description: ring_camera_row.get("description"),
+            snapshot: RingCameraSnapshot {
+                image: ring_camera_row.get("snapshot_image"),
+                timestamp: ring_camera_row.get("snapshot_timestamp"),
+            },
+            health: ring_camera_row.get("health"),
+            videos: VideoSearchRes {
+                video_search: Vec::new(),
+            },
         });
     }
 
-    let (mut locations, ring_devices) = tokio::join!(
-        ring_rest_client.get_locations(),
-        ring_rest_client.get_devices()
-    );
-    let mut cameras = Vec::with_capacity(20);
-    let location = locations.user_locations.remove(0);
-
-    let doorbots = ring_devices
-        .doorbots
-        .into_iter()
-        .chain(ring_devices.authorized_doorbots.into_iter())
-        .collect::<Vec<_>>();
-
-    for doorbot in doorbots.iter() {
-        cameras.push(get_ring_camera(&ring_rest_client, doorbot).await)
-    }
-
-    let ws_url = "".to_string();
-
     Ok(DashboardValues {
-        location_name: location.name,
+        location_name: "".to_string(),
         cameras,
-        ws_url,
+        ws_url: "".to_string(),
         devices,
     })
 }
@@ -95,7 +81,7 @@ pub fn DashboardPage() -> impl IntoView {
             </div>
         </main>
 
-        <aside class="bg-gray-100 fixed inset-y-0 left-20 hidden w-96 overflow-y-auto border-r border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block space-y-0.5">
+        <aside class="bg-gray-100 fixed inset-y-0 left-20 hidden w-96 overflow-y-auto border-r border-gray-200 px-4 py-6 sm:px-6 lg:px-4 xl:block space-y-0.5">
             <DeviceList ring_values=dashboard_values/>
         </aside>
     }
