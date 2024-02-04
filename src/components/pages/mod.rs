eedf266ceff2ab5f11ff75893f67b6213c85b7a8
jiyuu-jin <zach@valist.io>
2024-02-03 23:39:10 -0500

    Add page for scripts

--- src/components/pages/mod.rs
+++ src/components/pages/mod.rs
@@ -1,6 +1,7 @@
-pub mod accounts_page;
 pub mod dashboard_page;
 pub mod devices_page;
+pub mod integrations_page;
 pub mod login_page;
+pub mod scripts_page;
 pub mod settings_page;
 pub mod websocket_page;
---
+++ src/components/pages/scripts_page.rs
@@ -0,0 +1,10 @@
+use leptos::*;
+
+#[component]
+pub fn ScriptsPage() -> impl IntoView {
+    view! {
+        <main class="lg:p-40 lg:pt-20 cursor-pointer">
+            <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">Scripts</div>
+        </main>
+    }
+}
