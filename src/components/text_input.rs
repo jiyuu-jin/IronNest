d75b37c57d2c3bfad97d43ed19a5078736e30323
jiyuu-jin <zach@valist.io>
2024-02-16 21:22:48 -0500

    Add text_input component

--- src/components/text_input.rs
+++ src/components/text_input.rs
@@ -8,20 +8,20 @@ pub fn TextInput(
     input_type: String,
 ) -> impl IntoView {
     view! {
-      <div>
-        <label for={&name} class="block text-sm font-medium leading-6 text-white">
-            {label}
-        </label>
-          <div class="mt-2">
-              <input
-                type={input_type}
-                id={&name}
-                name={&name}
-                placeholder={placeholder}
-                required
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-          </div>
-      </div>
+        <div>
+            <label for=&name class="block text-sm font-medium leading-6 text-white">
+                {label}
+            </label>
+            <div class="mt-2">
+                <input
+                    type=input_type
+                    id=&name
+                    name=&name
+                    placeholder=placeholder
+                    required
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+            </div>
+        </div>
     }
 }
