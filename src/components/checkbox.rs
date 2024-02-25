c5c33993bf857e5ca7e54369045dceb04579e3ef
jiyuu-jin <zach@valist.io>
2024-02-24 21:49:45 -0500

    Lint components

--- src/components/checkbox.rs
+++ src/components/checkbox.rs
@@ -11,14 +11,16 @@ pub fn Checkbox(value: bool, on_click: Box<dyn Fn(bool)>) -> impl IntoView {
                 e.stop_propagation();
             }
         >
+
             <input
                 type="checkbox"
-                checked={move || signal.get()}
+                checked=move || signal.get()
                 on:click:undelegated=move |ev| {
                     log!("clicked!");
                     set_signal.set(!signal.get());
                     on_click(event_target_checked(&ev));
                 }
+
                 class="sr-only peer"
             />
             <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
--- src/components/select.rs
+++ src/components/select.rs
@@ -1,31 +1,29 @@
 use leptos::*;
 
 #[component]
-pub fn Select(label: String, name: String, data: Vec<String>) -> impl IntoView {
+pub fn Select<'a>(label: String, name: String, data: Vec<&'a str>) -> impl IntoView {
     view! {
-      <div>
-        <label
-            for="function-name"
-            class="block text-sm font-medium leading-6 text-gray-900"
-        >
-          {label}
-        </label>
-        <div class="inline">
-            <div class="mt-2 inline">
-                <select
-                    id={name.clone()}
-                    name={name}
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                >
-                {data.iter().map(|value| {
-                    view! {
-                      <option>{value}</option>
-                    }
-                  }).collect::<Vec<_>>()
-                }
-                </select>
+        <div>
+            <label for="function-name" class="block text-sm font-medium leading-6 text-gray-900">
+                {label}
+            </label>
+            <div class="inline">
+                <div class="mt-2 inline">
+                    <select
+                        id=name.clone()
+                        name=name
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                        {data
+                            .iter()
+                            .map(|value| {
+                                view! { <option>{value.to_string()}</option> }
+                            })
+                            .collect::<Vec<_>>()}
+
+                    </select>
+                </div>
             </div>
         </div>
-      </div>
     }
 }
