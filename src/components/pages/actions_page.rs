5b04a0d8a2f916b0b4d89425be83c905629143ef
jiyuu-jin <zach@valist.io>
2024-04-05 22:17:34 -0400

    Update action page content

--- src/components/pages/actions_page.rs
+++ src/components/pages/actions_page.rs
@@ -1,4 +1,21 @@
-use leptos::*;
+use {
+    crate::components::{select::Select, text_input::TextInput},
+    leptos::*,
+};
+
+// #[server(HandleCreateAction)]
+// pub async fn handle_ring_login(
+//     username: String,
+//     password: String,
+//     tfa: String,
+// ) -> Result<String, ServerFnError> {
+//     use crate::integrations::iron_nest::execute_function;
+//     let function_name = tool_call.function.name.to_string();
+//     let function_args: serde_json::Value = tool_call.function.arguments.parse().unwrap();
+//     let function_response = execute_function(function_name, function_args).await;
+
+//     Ok(result)
+// }
 
 #[component]
 pub fn ActionsPage() -> impl IntoView {
@@ -61,26 +78,15 @@ pub fn ActionsPage() -> impl IntoView {
                                             <div class="flex flex-1 flex-col justify-between">
                                                 <div class="divide-y divide-gray-200 px-4 sm:px-6">
                                                     <div class="space-y-6 pb-5 pt-6">
-                                                        <div>
-                                                            <label
-                                                                for="event-name"
-                                                                class="block text-sm font-medium leading-6 text-gray-900"
-                                                            >
-                                                                "Event name"
-                                                            </label>
-                                                            <div class="mt-2">
-                                                                <input
-                                                                    type="text"
-                                                                    name="project-name"
-                                                                    id="project-name"
-                                                                    placeholder="New event"
-                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                                                />
-                                                            </div>
-                                                        </div>
+                                                        <TextInput
+                                                            label="Event Name".to_owned()
+                                                            name="name".to_owned()
+                                                            placeholder="New event".to_owned()
+                                                            input_type="text".to_owned()
+                                                        />
                                                         <fieldset>
                                                             <legend class="text-sm font-medium leading-6 text-gray-900">
-                                                                Schedule
+                                                                Repeat
                                                             </legend>
                                                             <div class="mt-2 space-y-4">
                                                                 <div class="relative flex items-start">
@@ -99,7 +105,7 @@ pub fn ActionsPage() -> impl IntoView {
                                                                             for="privacy-public"
                                                                             class="font-medium text-gray-900"
                                                                         >
-                                                                            "Repeat Daily"
+                                                                            "Repeat"
                                                                             <div class="inline">
                                                                                 <div class="mt-2 inline">
                                                                                     <select
@@ -144,30 +150,18 @@ pub fn ActionsPage() -> impl IntoView {
                                                                         </div>
                                                                     </div>
                                                                 </div>
-                                                                <div>
-                                                                    <label
-                                                                        for="function-name"
-                                                                        class="block text-sm font-medium leading-6 text-gray-900"
-                                                                    >
-                                                                        "Function name"
-                                                                    </label>
-                                                                    <div class="inline">
-                                                                        <div class="mt-2 inline">
-                                                                            <select
-                                                                                id="function-name"
-                                                                                name="function-name"
-                                                                                autocomplete="function-name"
-                                                                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                                                                            >
-                                                                                <option>"tplink_set_light_brightness"</option>
-                                                                                <option>"tplink_turn_light_on_off"</option>
-                                                                                <option>"tplink_turn_plug_on"</option>
-                                                                                <option>"tplink_turn_plug_off"</option>
-                                                                                <option>"stoplight_toggle"</option>
-                                                                            </select>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
+                                                                <Select
+                                                                    label="Function name".to_string()
+                                                                    name="function-name".to_string()
+                                                                    data=vec![
+                                                                        "tplink_set_light_brightness",
+                                                                        "tplink_turn_light_on_off",
+                                                                        "tplink_turn_plug_on",
+                                                                        "tplink_turn_plug_off",
+                                                                        "stoplight_toggle",
+                                                                    ]
+                                                                />
+
                                                             </div>
                                                         </fieldset>
                                                     </div>
