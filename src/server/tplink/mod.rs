137e9f1000bfd4c8f3d319a5dae35fb3e0e945a4
Chris Smith <chris@smith.xyz>
2024-11-03 17:06:10 -0500

    fix: inverted tplink plug and light

--- src/server/tplink/mod.rs
+++ src/server/tplink/mod.rs
@@ -4,9 +4,9 @@ use leptos::*;
 pub async fn handle_smart_plug_toggle(state: bool, ip: String) -> Result<(), ServerFnError> {
     use crate::integrations::tplink::{tplink_turn_plug_off, tplink_turn_plug_on};
     if state {
-        tplink_turn_plug_off(&ip).await;
-    } else {
         tplink_turn_plug_on(&ip).await;
+    } else {
+        tplink_turn_plug_off(&ip).await;
     }
     Ok(())
 }
@@ -14,7 +14,7 @@ pub async fn handle_smart_plug_toggle(state: bool, ip: String) -> Result<(), Ser
 #[server(HandleSmartLightToggle)]
 pub async fn handle_smart_light_toggle(state: bool, ip: String) -> Result<(), ServerFnError> {
     use crate::integrations::tplink::tplink_turn_light_on_off;
-    tplink_turn_light_on_off(&ip, if state { 0 } else { 1 }).await;
+    tplink_turn_light_on_off(&ip, if state { 1 } else { 0 }).await;
     Ok(())
 }
 
