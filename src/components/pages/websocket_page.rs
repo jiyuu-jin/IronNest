bee108752112903c729229005bb14237f6e4c0a7
jiyuu-jin <zach@valist.io>
2024-11-18 20:57:54 -0500

    Disable websocket test page until updated for latest leptos hooks

--- src/components/pages/websocket_page.rs
+++ src/components/pages/websocket_page.rs
@@ -1,13 +1,6 @@
 use {
     super::dashboard_page::DashboardValues,
-    crate::components::pages::dashboard_page::get_dashboard_values,
-    js_sys::Reflect,
-    leptos::*,
-    leptos_reactive::create_effect,
-    leptos_use::{core::ConnectionReadyState, use_websocket, UseWebsocketReturn},
-    serde_json::json,
-    wasm_bindgen::{closure::Closure, JsValue},
-    web_sys::RtcPeerConnection,
+    crate::components::pages::dashboard_page::get_dashboard_values, leptos::*,
 };
 
 #[component]
@@ -40,85 +33,85 @@ pub fn WebSocketPage() -> impl IntoView {
 
 #[component]
 fn WebSocketComponent(ring_values: DashboardValues) -> impl IntoView {
-    let UseWebsocketReturn {
-        ready_state,
-        message,
-        message_bytes,
-        send,
-        send_bytes,
-        open,
-        close,
-        ..
-    } = use_websocket(&ring_values.ws_url);
+    // let UseWebsocketReturn {
+    //     ready_state,
+    //     message,
+    //     message_bytes,
+    //     send,
+    //     send_bytes,
+    //     open,
+    //     close,
+    //     ..
+    // } = use_websocket(&ring_values.ws_url);
 
-    let send_message = move |_| {
-        send("Hello, world!");
-    };
+    // let send_message = move |_| {
+    //     send("Hello, world!");
+    // };
 
     // let send_byte_message = move |_| {
     //     send_bytes(b"Hello, world!\r\n".to_vec());
     // };
 
-    create_effect(move |_| {
-        if ready_state.get() == ConnectionReadyState::Open {
-            let send_bytes = send_bytes.clone();
-            let pc = RtcPeerConnection::new().unwrap();
-            let create_offer_callback = Closure::wrap(Box::new(move |offer: JsValue| {
-                let sdp = Reflect::get(&offer, &JsValue::from_str("sdp"))
-                    .unwrap()
-                    .as_string()
-                    .unwrap();
-                send_bytes(
-                    serde_json::to_vec(&json!({
-                        "method": "live_view",
-                        "dialog_id": "333333",
-                        "body": {
-                            "doorbot_id": "",
-                            "stream_options": { "audio_enabled": true, "video_enabled": true },
-                            "sdp": sdp,
-                        }
-                    }))
-                    .unwrap(),
-                );
-            }) as Box<dyn FnMut(JsValue)>);
-            let _ = pc.create_offer().then(&create_offer_callback);
-            create_offer_callback.forget();
-        }
-    });
-    let status = move || ready_state.get().to_string();
+    // create_effect(move |_| {
+    //     if ready_state.get() == ConnectionReadyState::Open {
+    //         let send_bytes = send_bytes.clone();
+    //         let pc = RtcPeerConnection::new().unwrap();
+    //         let create_offer_callback = Closure::wrap(Box::new(move |offer: JsValue| {
+    //             let sdp = Reflect::get(&offer, &JsValue::from_str("sdp"))
+    //                 .unwrap()
+    //                 .as_string()
+    //                 .unwrap();
+    //             send_bytes(
+    //                 serde_json::to_vec(&json!({
+    //                     "method": "live_view",
+    //                     "dialog_id": "333333",
+    //                     "body": {
+    //                         "doorbot_id": "",
+    //                         "stream_options": { "audio_enabled": true, "video_enabled": true },
+    //                         "sdp": sdp,
+    //                     }
+    //                 }))
+    //                 .unwrap(),
+    //             );
+    //         }) as Box<dyn FnMut(JsValue)>);
+    //         let _ = pc.create_offer().then(&create_offer_callback);
+    //         create_offer_callback.forget();
+    //     }
+    // });
+    // let status = move || ready_state.get().to_string();
 
-    let connected = move || ready_state.get() == ConnectionReadyState::Open;
+    // let connected = move || ready_state.get() == ConnectionReadyState::Open;
 
-    let open_connection = move |_| {
-        open();
-    };
+    // let open_connection = move |_| {
+    //     open();
+    // };
 
-    let close_connection = move |_| {
-        close();
-    };
+    // let close_connection = move |_| {
+    //     close();
+    // };
 
     view! {
         <div>
-            <p>"status: " {status}</p>
+            // <p>"status: " {status}</p>
 
-            <button on:click=send_message disabled=move || !connected()>
-                "Send"
-            </button>
-            // <button on:click=send_byte_message disabled=move || !connected()>
-            // "Send bytes"
+            // <button on:click=send_message disabled=move || !connected()>
+            //     "Send"
             // </button>
-            <button on:click=open_connection disabled=connected>
-                "Open"
-            </button>
-            <button on:click=close_connection disabled=move || !connected()>
-                "Close"
-            </button>
+            // // <button on:click=send_byte_message disabled=move || !connected()>
+            // // "Send bytes"
+            // // </button>
+            // <button on:click=open_connection disabled=connected>
+            //     "Open"
+            // </button>
+            // <button on:click=close_connection disabled=move || !connected()>
+            //     "Close"
+            // </button>
 
-            <h2>"Receive message: "</h2>
-            <pre style="text-wrap: wrap; word-break: break-all;">
-                {move || format!("{:?}", message.get())}
-            </pre>
-            <p>"Receive byte message: " {move || format!("{:?}", message_bytes.get())}</p>
+            // <h2>"Receive message: "</h2>
+            // <pre style="text-wrap: wrap; word-break: break-all;">
+            //     {move || format!("{:?}", message.get())}
+            // </pre>
+            // <p>"Receive byte message: " {move || format!("{:?}", message_bytes.get())}</p>
         </div>
     }
 }
