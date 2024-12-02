7f0618bd7af000a9833dd3ec54bb91becca83c91
jiyuu-jin <zach@valist.io>
2024-12-02 03:12:03 -0500

    Add mac_address and child_id to device_page query

--- src/components/pages/devices_page.rs
+++ src/components/pages/devices_page.rs
@@ -13,7 +13,7 @@ pub async fn get_devices() -> Result<Vec<Device>, ServerFnError> {
     let pool = use_context::<PgPool>().unwrap();
 
     let query = "
-        SELECT id, name, device_type, ip, power_state, battery_percentage, last_seen 
+        SELECT id, name, device_type, ip, power_state, battery_percentage, last_seen, mac_address, child_id 
         FROM device
     ";
     sqlx::query_as::<Postgres, Device>(query)
