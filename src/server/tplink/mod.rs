d1d5587dbdbfbdaee7e4e31f552eb868cfed77e2
jiyuu-jin <zach@valist.io>
2024-12-04 02:16:40 -0500

    Fix handle_smart_light_toggle device table should be devices

--- src/server/tplink/mod.rs
+++ src/server/tplink/mod.rs
@@ -66,7 +66,7 @@ pub async fn handle_smart_light_toggle(state: bool, ip: String) -> Result<(), Se
 
     let pool = use_context::<PgPool>().unwrap();
     let query = "
-        UPDATE devices
+        UPDATE device
         SET power_state = $1
         WHERE ip = $2
     ";
