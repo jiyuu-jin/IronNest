cc0b720d4ffd84d19c9b7aa82080430ded294fca
jiyuu-jin <zach@valist.io>
2025-01-03 15:11:31 -0500

    Move meal_planning types to instacart integration

--- src/integrations/instacart/types/mod.rs
+++ src/integrations/instacart/types/mod.rs
@@ -1,1 +1,13 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Ingredient {
+    pub name: String,
+    pub amount: String,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct ScheduledMeal {
+    pub recipie_name: String,
+    pub ingredients: Ingredient,
+}
