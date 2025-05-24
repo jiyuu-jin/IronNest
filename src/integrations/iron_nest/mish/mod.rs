0a48b6efd7acc28166a2adbe990fa829cc8fd28e
Chris Smith <chris@smith.xyz>
2025-05-24 09:37:51 -0400

    feat: functional blue light mish state

--- src/integrations/iron_nest/mish/mod.rs
+++ src/integrations/iron_nest/mish/mod.rs
@@ -1,4 +1,7 @@
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use {
+    crate::integrations::tplink::{tplink_turn_plug_off, tplink_turn_plug_on},
+    tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender},
+};
 
 #[derive(Debug, Clone)]
 pub enum MishStateModification {
@@ -22,6 +25,35 @@ pub async fn register_native_queries(
     mut mish_state_modification_bus_receiver: UnboundedReceiver<MishStateModification>,
 ) {
     while let Some(mish_state_modification) = mish_state_modification_bus_receiver.recv().await {
-        println!("Mish state modification: {:?}", mish_state_modification);
+        log::info!("Mish state modification: {:?}", mish_state_modification);
+        match mish_state_modification {
+            MishStateModification::CreateOrUpdate { name, state } => match name.as_str() {
+                "chris.fish_tank.filter.pump" => {
+                    // TODO do via mish
+                }
+                "chris.fish_tank.light.white" => {
+                    // TODO do via mish
+                }
+                "chris.fish_tank.light.blue" => {
+                    let blue_on_ip = "10.0.0.198";
+                    let state = serde_json::from_value::<bool>(state);
+                    match state {
+                        Ok(state) => {
+                            if state {
+                                tplink_turn_plug_on(blue_on_ip).await;
+                            } else {
+                                tplink_turn_plug_off(blue_on_ip).await;
+                            }
+                        }
+                        Err(_) => {
+                            log::error!("Failed to parse blue light state on: {:?}", state);
+                        }
+                    }
+                }
+                _ => {}
+            },
+            MishStateModification::Delete { name: _ } => {}
+        }
     }
+    // TODO listen to events first and then poll the current state in-case something was missed
 }
