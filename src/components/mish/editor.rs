e62dffc442865903ffe37486ebce4322d6942fa0
Chris Smith <chris@smith.xyz>
2025-07-27 18:35:57 -0400

    fix: raw editor switch

--- src/components/mish/editor.rs
+++ src/components/mish/editor.rs
@@ -7,84 +7,94 @@ use {
 };
 
 #[component]
-pub fn Editor(state: serde_json::Value, action: impl Fn(Vec<u8>) + 'static) -> impl IntoView {
+pub fn Editor(
+    state: serde_json::Value,
+    action: impl Fn(Vec<u8>) + 'static + Send + Sync,
+) -> impl IntoView {
+    let action = Arc::new(action);
     let raw_editor_mode = RwSignal::new(false);
 
-    let editor = if raw_editor_mode.get() {
-        view! { <JsonEditor state=Some(state) set_config_server_action=action /> }.into_any()
-    } else {
-        match state {
-            serde_json::Value::Bool(b) => view! {
-                <input
-                    type="checkbox"
-                    checked=b
-                    on:input:target=move |ev| {
-                        let value = ev.target().checked();
-                        action(serde_json::to_vec(&value).unwrap());
-                    }
-                />
-            }
-            .into_any(),
-            serde_json::Value::String(s) => view! {
-                <TextEditor
-                    state=s
-                    set_config_server_action=move |s| { action(serde_json::to_vec(&s).unwrap()) }
-                />
-            }
-            .into_any(),
-            serde_json::Value::Number(n) => {
-                view! { <NumberEditor state=n.to_string() set_config_server_action=action /> }
-                    .into_any()
-            }
-            serde_json::Value::Array(a) => {
-                let action = Arc::new(action);
-                a.iter()
-                    .enumerate()
-                    .map(|(i, v)| {
-                        let a = a.clone();
-                        let action = action.clone();
-                        view! {
-                            <div>
-                                <NestedEditor
-                                    state=v.clone()
-                                    action=Box::new(move |s| {
-                                        let mut a = a.clone();
-                                        a[i] = serde_json::from_slice(&s).unwrap();
-                                        action(serde_json::to_vec(&a).unwrap());
-                                    })
-                                />
-                            </div>
+    let editor = move || {
+        let state = state.clone();
+        if raw_editor_mode.get() {
+            let action = action.clone();
+            view! { <JsonEditor state=Some(state) set_config_server_action=move |s| { action(s) } /> }.into_any()
+        } else {
+            let action = action.clone();
+            match state {
+                serde_json::Value::Bool(b) => view! {
+                    <input
+                        type="checkbox"
+                        checked=b
+                        on:input:target=move |ev| {
+                            let value = ev.target().checked();
+                            action(serde_json::to_vec(&value).unwrap());
                         }
-                    })
-                    .collect::<Vec<_>>()
-                    .into_any()
-            }
-            serde_json::Value::Object(o) => {
-                let action = Arc::new(action);
-                o.clone()
-                    .into_iter()
-                    .map(|(k, v)| {
-                        let o = o.clone();
-                        let action = action.clone();
-                        view! {
-                            <div>
-                                <div>{k.clone()}</div>
-                                <NestedEditor
-                                    state=v.clone()
-                                    action=Box::new(move |s| {
-                                        let mut o = o.clone();
-                                        o.insert(k.clone(), serde_json::from_slice(&s).unwrap());
-                                        action(serde_json::to_vec(&o).unwrap());
-                                    })
-                                />
-                            </div>
-                        }
-                    })
-                    .collect::<Vec<_>>()
-                    .into_any()
-            }
-            _ => view! { <JsonEditor state=Some(state) set_config_server_action=action /> }
+                    />
+                }
                 .into_any(),
+                serde_json::Value::String(s) => view! {
+                    <TextEditor
+                        state=s
+                        set_config_server_action=move |s| { action(serde_json::to_vec(&s).unwrap()) }
+                    />
+                }
+                .into_any(),
+                serde_json::Value::Number(n) => {
+                    view! { <NumberEditor state=n.to_string() set_config_server_action=move |s| { action(s) } /> }
+                        .into_any()
+                }
+                serde_json::Value::Array(a) => {
+                    let action = Arc::new(action);
+                    a.iter()
+                        .enumerate()
+                        .map(|(i, v)| {
+                            let a = a.clone();
+                            let action = action.clone();
+                            view! {
+                                <p>Array: {i}</p>
+                                <div style="border-left: 5px solid black">
+                                    <NestedEditor
+                                        state=v.clone()
+                                        action=Box::new(move |s| {
+                                            let mut a = a.clone();
+                                            a[i] = serde_json::from_slice(&s).unwrap();
+                                            action(serde_json::to_vec(&a).unwrap());
+                                        })
+                                    />
+                                </div>
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .into_any()
+                }
+                serde_json::Value::Object(o) => {
+                    let action = Arc::new(action);
+                    o.clone()
+                        .into_iter()
+                        .map(|(k, v)| {
+                            let o = o.clone();
+                            let action = action.clone();
+                            view! {
+                                <div>Key: {k.clone()}</div>
+                                <div style="border-left: 5px solid black">
+                                    <NestedEditor
+                                        state=v.clone()
+                                        action=Box::new(move |s| {
+                                            let mut o = o.clone();
+                                            o.insert(k.clone(), serde_json::from_slice(&s).unwrap());
+                                            action(serde_json::to_vec(&o).unwrap());
+                                        })
+                                    />
+                                </div>
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .into_any()
+                }
+                _ => view! { <JsonEditor state=Some(state) set_config_server_action=move |s| { action(s) } /> }
+                    .into_any(),
+            }
         }
     };
 
@@ -100,7 +110,7 @@ pub fn Editor(state: serde_json::Value, action: impl Fn(Vec<u8>) + 'static) -> i
 #[component]
 pub fn NestedEditor(
     state: serde_json::Value,
-    action: Box<dyn Fn(Vec<u8>) + 'static>,
+    action: Box<dyn Fn(Vec<u8>) + 'static + Send + Sync>,
 ) -> impl IntoView {
     view! { <Editor state=state action=action /> }
 }
